b4987fc9e048bf4ef3ee551c95543d1863d2f2f9
Walter Schulze <awalterschulze@gmail.com>
2020-02-08 14:46:24 +0000

    better interface for tregex

--- tregex.v
+++ tregex.v
@@ -4,9 +4,19 @@ Require Import Program.Basics.
 Section Tregexes.
 
 Variable X: Set.
+Parameter compare : X -> X -> comparison.
+Parameter proof_compare_equal: forall (x y: X) (p: compare x y = Eq),
+  x = y.
+Parameter proof_compare_reflex: forall (x: X), compare x x = Eq.
+
+Definition is_eq (x y: X) : bool :=
+  match compare x y with
+  | Eq => true
+  | _ => false
+  end.
+
 Inductive tree :=
   tree_node : X -> list tree -> tree.
-Parameter is_eq : X -> X -> bool.
 
 (* 
 [a[a[]b[]] b[]]
