d87dab9470448ed6102b8d2ad5424decaedbb135
Paul Cadman <pcadman@gmail.com>
2020-05-09 16:53:05 +0100

    Adds an interpretation of a regex as a set of sequences.
    This leads to a simple definition of derivative.

---
+++ src/set_of_sequences.v
@@ -0,0 +1,44 @@
+Require Import List.
+Import ListNotations.
+Require Import Bool.
+
+Require Import comparable.
+Require Import nullable.
+Require Import regex.
+Require Import derive_def.
+
+Definition set_of_sequences {A: Type} {cmp: comparable A} := list A -> Prop.
+
+Notation "xs \in R" := (R xs) (at level 80).
+Notation "{ xs | P }" := (fun xs => P) (at level 0, xs at level 99).
+
+Reserved Notation "| r |" (at level 60).
+Reserved Notation "xs \in * R" (at level 80).
+
+Inductive matching_sequences_for_star {A: Type} {cmd: comparable A} (R: set_of_sequences): set_of_sequences :=
+| star_matches_nil : [] \in *R
+| star_matches_concat : forall xs ys,
+    xs \in R ->
+    ys \in *R ->
+    xs ++ ys \in *R
+where "xs \in * R" := ((matching_sequences_for_star R) xs).
+
+Fixpoint matching_sequences_for_regex {A: Type} {cmp: comparable A} (r: regex A): set_of_sequences :=
+  match r with
+  | fail => { _ | False }
+  | empty => { xs | xs = [] }
+  | char a => { xs | xs = [a] }
+  | or r1 r2 => { xs | xs \in |r1| \/ xs \in |r2| }
+  | and r1 r2 => { xs | xs \in |r1| /\ xs \in |r2| }
+  | concat r1 r2 => { xs | exists ys zs, xs = ys ++ xs /\ ys \in |r1| /\ zs \in |r2| }
+  | not r1 => { xs | ~ (xs \in |r1|) }
+  | star r1 => { xs | xs \in *|r1| }
+  end
+where "| r |" := (matching_sequences_for_regex r).
+
+Definition derive_sequence {A: Type} {cmd: comparable A} (a: A) (R: set_of_sequences) : set_of_sequences :=
+  { xs | (a :: xs) \in R }.
+
+Theorem derive_is_derivative {A: Type} {cmd: comparable A} (a: A) (r: regex A):
+  forall (xs: list A), xs \in |derive r a| <-> xs \in derive_sequence a (|r|).
+Admitted.
