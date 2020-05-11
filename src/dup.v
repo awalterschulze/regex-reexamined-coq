376a271d48fed83382db42f5cfd449be3c3612c1
Niels uit de Bos <niels.uitdebos@zopa.com>
2020-05-11 11:24:36 +0100

    Prove verified decision procedure for strictly sorted

--- src/dup.v
+++ src/dup.v
@@ -32,6 +32,69 @@ Section inductive_predicate_strictly_sorted.
     intro ls. intro H.
     destruct H; auto with strictly_sorted.
   Qed.
+
+  Local Ltac is_strictly_sorted_contradiction_via_tail :=
+    match goal with
+    | [ H: is_strictly_sorted (?x0 :: ?xs),
+           Hcon: ~(is_strictly_sorted ?xs)
+        |- False ]
+      => apply Hcon;
+         apply is_strictly_sorted_tail;
+         exact H
+    | [ H: ~(is_strictly_sorted ?xs)
+        |- ~(is_strictly_sorted (?x0 :: ?xs))]
+        (* in this case, just apply the contrapositive of is_strictly_sorted_tail *)
+      => contradict H;
+         apply is_strictly_sorted_tail with (ls := (x0 :: xs));
+         assumption
+    end.
+
+  Definition is_strictly_sorted_dec: forall (ls :list A),
+      {is_strictly_sorted ls} + {~(is_strictly_sorted ls)}.
+
+    Local Ltac extract_info_from_strictly_sorted :=
+      unfold not; intros; subst;
+      match goal with
+      | [ H: is_strictly_sorted (?x0 :: ?xs) |- _ ]
+        => inversion H; subst;
+           try discriminate (* this eliminates some impossible cases *)
+      end;
+      destruct_list_equality.
+
+    refine (
+        fix F (ls: list A) : {is_strictly_sorted ls} + {~(is_strictly_sorted ls)} :=
+          (match ls
+                 return {is_strictly_sorted ls} +
+                        {~(is_strictly_sorted ls)}
+           with
+             | nil => left _
+             | (x0::ls') =>
+               (match ls' as l
+                      return (ls' = l) ->
+                             {is_strictly_sorted (x0::ls')} +
+                             {~(is_strictly_sorted (x0::ls'))}
+                with
+                | nil => (fun _ => left _)
+                | (x1::ls'') =>
+                  (fun ls0 =>
+                     (match (compare x0 x1) as cmp
+                            return (compare x0 x1 = cmp) ->
+                                   {is_strictly_sorted (x0::ls')} +
+                                   {~(is_strictly_sorted (x0::ls'))}
+                      with
+                      | Lt =>
+                        (fun Hcmp => if (F ls')
+                                     then left _
+                                     else right _)
+                      | _ =>
+                        (fun Hcmp => right _)
+                      end) eq_refl)
+                end) eq_refl
+           end));
+      try (subst; constructor; assumption);
+      try is_strictly_sorted_contradiction_via_tail;
+      try (extract_info_from_strictly_sorted; contradiction_from_compares).
+    Defined.
 End inductive_predicate_strictly_sorted.
 
 Section remove_duplicates_from_sorted.
@@ -45,18 +108,18 @@ Section remove_duplicates_from_sorted.
       is_sorted ls -> {ls' : list A | is_strictly_sorted ls'}.
     intro.
     (* Probably need to do convoy trick here again. *)
-    refine (match ls with
-              | nil => (exist _ nil empty_strictly_sorted)
-              | (x0 :: xs')
-                => match xs' with
-                   | nil => (exist _ [x0] (singleton_strictly_sorted x0))
-                   | (x1 :: xs'') =>
-                     match compare x0 x1 with
-                     | Eq => remove_duplicates_from_sorted xs' (tail_of_is_sorted_is_sorted H)
-                     | _ => x0 :: (remove_duplicates_from_sorted xs' (tail_of_is_sorted_is_sorted H))
-                     end
-                   end
-            end).
+    (* refine (match ls with *)
+    (*           | nil => (exist _ nil empty_strictly_sorted) *)
+    (*           | (x0 :: xs') *)
+    (*             => match xs' with *)
+    (*                | nil => (exist _ [x0] (singleton_strictly_sorted x0)) *)
+    (*                | (x1 :: xs'') => *)
+    (*                  match compare x0 x1 with *)
+    (*                  | Eq => remove_duplicates_from_sorted xs' (tail_of_is_sorted_is_sorted H) *)
+    (*                  | _ => x0 :: (remove_duplicates_from_sorted xs' (tail_of_is_sorted_is_sorted H)) *)
+    (*                  end *)
+    (*                end *)
+    (*         end). *)
   Abort.
 End remove_duplicates_from_sorted.
 
