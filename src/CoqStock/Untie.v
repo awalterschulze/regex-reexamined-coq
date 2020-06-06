37f644ca8947d7bf350ed7755d80727ab5c70e81
Walter Schulze <awalterschulze@gmail.com>
2020-06-06 16:40:38 +0100

    more targeted rewriting of not

--- src/CoqStock/Untie.v
+++ src/CoqStock/Untie.v
@@ -11,24 +11,64 @@ It then turns the expression inside the not, into an hypothesis
 H: x
 False
 ```
-Then it inverts the new hypothesis H and clears it.
+It attempts to rewrite with the hypothesis, if possible.
+If rewrite succeeds, the hypothesis is cleared.
+Finally tries to apply discriminate and contradiction.
 *)
 
-Local Ltac untie_goal :=
-let H := fresh "H"
-in unfold not;
-   intros H;
-   inversion_clear H;
-   subst.
-
-Ltac untie :=
+Ltac untie_step :=
   match goal with
+  | [ H: context [?X] |- ?X <> _ ] =>
+    let Heq := fresh "Heq"
+    in unfold not; 
+       intro Heq;
+       rewrite Heq in *;
+       clear Heq
+  | [ H: context [?X] |- _ <> ?X ] =>
+    let Heq := fresh "Heq"
+    in unfold not;
+       intro Heq;
+       rewrite <- Heq in *;
+       clear Heq
   | [ |- ~ _ ] =>
-    untie_goal
+    unfold not; intro
+  | [ |- _ ] =>
+    discriminate || contradiction
   end.
+
+Ltac untie := repeat untie_step.
+
+Example example_subst_0: forall (x: nat),
+  x = 1 -> x <> 2.
+Proof.
+intros.
+untie.
+Qed.
+
+Example example_subst_1: forall (x: nat),
+ 1 = x -> x <> 2.
+Proof.
+intros.
+untie.
+Qed.
+
+Example example_subst_2: forall (x: nat),
+ 1 = x -> 2 <> x.
+Proof.
+intros.
+untie.
+Qed.
+
+Example example_subst_3: forall (x: nat),
+ x = 1 -> 2 <> x.
+Proof.
+intros.
+untie.
+Qed.
 
 Example example_untie_neq: 5 <> 4.
 Proof.
+intros.
 untie.
 Qed.
 
