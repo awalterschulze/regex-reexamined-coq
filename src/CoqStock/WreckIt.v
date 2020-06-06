78c67599399781e99b615c539a9917e7a4fa4db3
Walter Schulze <awalterschulze@gmail.com>
2020-06-06 17:34:42 +0100

    make wreckit do more targeted rewrites

--- src/CoqStock/WreckIt.v
+++ src/CoqStock/WreckIt.v
@@ -48,8 +48,13 @@ wreckit is a tactic to break down:
 *)
 Ltac inversion_exists :=
 match goal with
+| [ H: exists _, _ = _ |- _ ] => 
+  let E := fresh "E"
+  in let B := fresh "B"
+  in inversion_clear H as [E B];
+  try rewrite B in *
 | [ H: exists _, _ |- _ ] => 
-  inversion_clear H; subst
+  inversion_clear H
 end.
 
 Example example_inversion_exists: forall (x: nat) (e: exists (y: nat), x = S y /\ y = O),
@@ -60,6 +65,14 @@ inversion_exists.
 inversion_clear H.
 subst.
 reflexivity.
+Qed.
+
+Example example_inversion_exists_neq: forall (x: nat) (e: exists (y: nat), x = S y),
+  x <> O.
+Proof.
+intros.
+inversion_exists.
+discriminate.
 Qed.
 
 (* inversion_conj:
@@ -76,7 +89,11 @@ Qed.
 Ltac inversion_conj :=
 match goal with
 | [ H: _ /\ _ |- _ ] =>
-  inversion_clear H; subst
+  let L := fresh "L"
+  in let R := fresh "R"
+  in inversion_clear H as [L R];
+  try rewrite L in *;
+  try rewrite R in *
 end.
 
 Example example_inversion_conj: forall (x: nat) (e: exists (y: nat), x = S y /\ y = O),
@@ -102,18 +119,16 @@ Qed.
 Ltac inversion_disj :=
 match goal with
 | [ H: _ \/ _ |- _ ] =>
-  inversion_clear H
+  let B := fresh "B"
+  in inversion_clear H as [B | B];
+     try rewrite B in *
 end.
 
 Example example_inversion_disj: forall (x: nat) (p: x = 0 \/ x = 1),
   x < 2.
 Proof.
 intros.
-inversion_disj.
-- subst.
-  auto.
-- subst.
-  auto.
+inversion_disj; auto.
 Qed.
 
 (* constructor_conj
@@ -137,9 +152,9 @@ Example example_constructor_conj: forall (x: nat) (p: x = 0),
 Proof.
 intros.
 constructor_conj.
-- subst.
+- rewrite p.
   auto.
-- subst.
+- rewrite p.
   auto.
 Qed.
 
@@ -158,4 +173,11 @@ Example example_wreckit: forall (x: nat) (e: exists (y: nat), x = S y /\ y = O),
 Proof.
 intros.
 wreckit; reflexivity.
+Qed.
+
+Example example_wreckit_disj: forall (x: nat) (e: exists (y: nat), (x = S y \/ S y = x) /\ y = O),
+  x = S O \/ S O = x.
+Proof.
+intros.
+wreckit; auto.
 Qed.
