6c597e5e15a410afa9b4cd411195d7d43424cb8e
Walter Schulze <awalterschulze@gmail.com>
2020-06-14 09:11:15 +0100

    rename wreck local tactics

--- src/CoqStock/WreckIt.v
+++ src/CoqStock/WreckIt.v
@@ -34,7 +34,7 @@ wreckit is a tactic to break down:
   - conjuction in the goal
 *)
 
-(* destruct_exists:
+(* wreck_exists:
   - finds an exists in the hypotheses 
   - inverts that hypothesis
   - removes that hypothesis and 
@@ -46,7 +46,7 @@ wreckit is a tactic to break down:
   H: ?Y
   ```
 *)
-Ltac destruct_exists :=
+Ltac wreck_exists :=
 match goal with
 | [ H: exists _, _ = _ |- _ ] => 
   let E := fresh "E"
@@ -57,25 +57,25 @@ match goal with
   destruct H
 end.
 
-Example example_destruct_exists: forall (x: nat) (e: exists (y: nat), x = S y /\ y = O),
+Example example_wreck_exists: forall (x: nat) (e: exists (y: nat), x = S y /\ y = O),
   x = S O.
 Proof.
 intros.
-destruct_exists.
+wreck_exists.
 inversion_clear H.
 subst.
 reflexivity.
 Qed.
 
-Example example_destruct_exists_neq: forall (x: nat) (e: exists (y: nat), x = S y),
+Example example_wreck_exists_neq: forall (x: nat) (e: exists (y: nat), x = S y),
   x <> O.
 Proof.
 intros.
-destruct_exists.
+wreck_exists.
 discriminate.
 Qed.
 
-(* destruct_conj:
+(* wreck_conj:
   - finds a conjunction in the hypotheses
   - inverts that hypothesis
   - clears that hypothesis
@@ -86,7 +86,7 @@ Qed.
   H1: ?Y
   ```
 *)
-Ltac destruct_conj :=
+Ltac wreck_conj :=
 match goal with
 | [ H: _ /\ _ |- _ ] =>
   let L := fresh "L"
@@ -96,12 +96,12 @@ match goal with
   try rewrite R in *
 end.
 
-Example example_destruct_conj: forall (x: nat) (e: exists (y: nat), x = S y /\ y = O),
+Example example_wreck_conj: forall (x: nat) (e: exists (y: nat), x = S y /\ y = O),
   x = S O.
 Proof.
 intros.
-destruct_exists.
-destruct_conj.
+wreck_exists.
+wreck_conj.
 reflexivity.
 Qed.
 
@@ -116,7 +116,7 @@ Qed.
      - H2: ?Y
   ```
 *)
-Ltac destruct_disj :=
+Ltac wreck_disj :=
 match goal with
 | [ H: _ \/ _ |- _ ] =>
   let B := fresh "B"
@@ -124,11 +124,11 @@ match goal with
      try rewrite B in *
 end.
 
-Example example_destruct_disj: forall (x: nat) (p: x = 0 \/ x = 1),
+Example example_wreck_disj: forall (x: nat) (p: x = 0 \/ x = 1),
   x < 2.
 Proof.
 intros.
-destruct_disj; auto.
+wreck_disj; auto.
 Qed.
 
 Theorem conj2: forall (P: Prop),
@@ -182,9 +182,9 @@ Qed.
 
 (* wreckit_step is helpful for seeing what wreckit does step by step *)
 Ltac wreckit_step :=
-     destruct_exists
-  || destruct_conj
-  || destruct_disj
+     wreck_exists
+  || wreck_conj
+  || wreck_disj
   || constructor_conj
   .
 
