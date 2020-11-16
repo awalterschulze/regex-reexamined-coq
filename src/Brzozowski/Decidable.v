a9074906e246c27c61d9b9751073264777e84d6b
Walter Schulze <awalterschulze@gmail.com>
2020-11-16 08:31:52 +0000

    move concat above star to have a predictable order

--- src/Brzozowski/Decidable.v
+++ src/Brzozowski/Decidable.v
@@ -94,6 +94,157 @@ wreckit.
   * assumption.
 Qed.
 
+Definition no_splitting_is_an_elem_for_length (p q: regex) (s: str) (n: nat) :=
+  forall (s1 s2: str),
+  s = s1 ++ s2 ->
+  length s1 <= n ->
+  ((s1 `notelem` {{ p }} \/ s2 `notelem` {{ q }})).
+
+Definition a_splitting_is_an_elem_for_length (p q: regex) (s: str) (n: nat) :=
+  exists (s1 s2: str),
+  s = s1 ++ s2 /\
+  length s1 <= n /\
+  (s1 `elem` {{ p }} /\ s2 `elem` {{ q }}).
+
+Lemma denotation_concat_is_decidable_helper (p q: regex):
+  regex_is_decidable p ->
+  regex_is_decidable q ->
+  (forall (s: str) (n : nat),
+    no_splitting_is_an_elem_for_length p q s n
+    \/ a_splitting_is_an_elem_for_length p q s n
+  ).
+Proof.
+  intros Hdecp Hdecq s n.
+  induction n.
+  - (* case that s1 is empty string *)
+    destruct (Hdecp []) as [Hpmatches | Hpnomatch]; destruct (Hdecq s) as [Hqmatches | Hqnomatch].
+
+
+    2,3,4: left; intros s1 s2 Hconcat Hlen';
+      (* this could maybe use some refactoring.
+         But in principle it is simple: I want to do exactly this
+         for goals 2,3 and 4.
+       *)
+      (* now starts: we know what it is when it is split *)
+      assert (s1 = []) by (apply length_zero_or_smaller_string_is_empty; assumption);
+      assert (s2 = s) by (replace (s1 ++ s2) with s2 in Hconcat by (subst; auto);
+                          symmetry;
+                          assumption);
+      clear Hconcat;
+      clear Hlen';
+      subst;
+      try (now left);
+      try (now right).
+
+    + (* this is the case where in fact s matches q, [] matches p *)
+      right.
+      exists [].
+      exists s.
+      intros.
+      auto.
+
+  - (* induction step *)
+    set (l1 := firstn (S n) s).
+    set (l2 := skipn (S n) s).
+
+    (* case distinction on the induction hyptohesis (which is an or) *)
+    destruct IHn as [IHnAllNoMatch | IHnExistsMatch ].
+
+    (* The case where there is already a match with a smaller split. *)
+    2: {
+    right.
+    destruct IHnExistsMatch as [s1 IHn1].
+    destruct IHn1 as [s2 IHn].
+    exists s1. exists s2.
+    destruct IHn as [H0 [H1 [H2 H3]]].
+    repeat split; try assumption.
+    lia. }
+
+
+    (* If none of the earlier splits match. *)
+    destruct (Hdecp l1) as [Hpmatch | Hpnomatch].
+      destruct (Hdecq l2) as [Hqmatch | Hqnomatch ].
+
+      2,3: left;
+      intros s1 s2 Hconcat Hlen;
+      assert (length s1 <= n \/ length s1 = S n) as Hlen' by lia;
+      destruct Hlen' as [Hlen' | Hlen'];
+      try (apply IHnAllNoMatch; assumption); (* case length s1 <= n *)
+      try (
+          destruct (split_list s (S n) s1 s2 Hlen' Hconcat) as [Hfoo Hbar];
+
+          replace l1 with s1 in * by auto;
+          replace l2 with s2 in * by auto;
+          subst;
+          try (left; assumption);
+          try (right; assumption)).
+
+
+    + right. exists l1. exists l2. intros.
+
+      repeat split; try assumption.
+
+      * symmetry. apply firstn_skipn.
+      * apply firstn_le_length.
+
+
+        (* The proof below is the proof for 2,3, but written with periods instead of semicolons...
+           the periods are easier to step through, and it is also how I wrote it.
+           But as I've said above, I don't know how to do that for 2 goals at the same time.
+
+         *)
+        (*
+    +
+      left.
+      intros s1 s2 Hconcat Hlen.
+
+      assert (length s1 <= n \/ length s1 = S n) as Hlen' by lia.
+      destruct Hlen' as [Hlen' | Hlen'].
+
+      * apply IHnAllNoMatch; assumption. (* case length s1 <= n *)
+      * destruct (split_string_lemma s (S n) s1 s2 Hlen' Hconcat) as [Hfoo Hbar].
+
+        replace l1 with s1 in * by auto.
+        replace l2 with s2 in * by auto.
+        subst.
+        try (left; assumption).
+        try (right; assumption).
+*)
+
+Qed.
+
+Lemma denotation_concat_is_decidable (p q: regex):
+  regex_is_decidable p ->
+  regex_is_decidable q ->
+  regex_is_decidable (concat p q).
+Proof.
+  intros Hdecp Hdecq.
+  unfold regex_is_decidable.
+  intro s.
+  destruct (denotation_concat_is_decidable_helper p q Hdecp Hdecq s (length s))
+  as [HAllDontMatch | HExistsMatch].
+
+  - right.
+    unfold not.
+    intro HmatchContr.
+    destruct HmatchContr.
+    symmetry in H.
+
+    set (Hlen := prefix_leq_length s p0 q0 H).
+    unfold no_splitting_is_an_elem_for_length in HAllDontMatch.
+    specialize HAllDontMatch with p0 q0.
+    destruct (HAllDontMatch H Hlen); auto.
+
+  - left.
+    destruct HExistsMatch as [s1 [s2 [Hconcat [Hlen [Hmatchp Hmatchq]]]]].
+    symmetry in Hconcat.
+    destruct_concat_lang.
+    exists s1.
+    exists s2.
+    exists Hconcat.
+    split; assumption.
+Qed.
+
 Definition elem_of_split (r: regex) (s: str) (index: nat) :=
   let prefix := firstn index s in
   let suffix := skipn index s in
@@ -364,214 +515,6 @@ intro Hr.
 intro s.
 apply (denotation_star_is_decidable_bounded_len r (length s) Hr).
 lia.
-Qed.
-
-Definition no_splitting_is_an_elem_for_length (p q: regex) (s: str) (n: nat) :=
-  forall (s1 s2: str),
-  s = s1 ++ s2 ->
-  length s1 <= n ->
-  ((s1 `notelem` {{ p }} \/ s2 `notelem` {{ q }})).
-
-Definition a_splitting_is_an_elem_for_length (p q: regex) (s: str) (n: nat) :=
-  exists (s1 s2: str),
-  s = s1 ++ s2 /\
-  length s1 <= n /\
-  (s1 `elem` {{ p }} /\ s2 `elem` {{ q }}).
-
-Lemma denotation_concat_is_decidable_helper (p q: regex):
-  regex_is_decidable p ->
-  regex_is_decidable q ->
-  (forall (s: str) (n : nat),
-    no_splitting_is_an_elem_for_length p q s n
-    \/ a_splitting_is_an_elem_for_length p q s n
-  ).
-Proof.
-  intros Hdecp Hdecq s n.
-  induction n.
-  - (* case that s1 is empty string *)
-    destruct (Hdecp []) as [Hpmatches | Hpnomatch]; destruct (Hdecq s) as [Hqmatches | Hqnomatch].
-
-
-    2,3,4: left; intros s1 s2 Hconcat Hlen';
-      (* this could maybe use some refactoring.
-         But in principle it is simple: I want to do exactly this
-         for goals 2,3 and 4.
-       *)
-      (* now starts: we know what it is when it is split *)
-      assert (s1 = []) by (apply length_zero_or_smaller_string_is_empty; assumption);
-      assert (s2 = s) by (replace (s1 ++ s2) with s2 in Hconcat by (subst; auto);
-                          symmetry;
-                          assumption);
-      clear Hconcat;
-      clear Hlen';
-      subst;
-      try (now left);
-      try (now right).
-
-    + (* this is the case where in fact s matches q, [] matches p *)
-      right.
-      exists [].
-      exists s.
-      intros.
-      auto.
-
-  - (* induction step *)
-    set (l1 := firstn (S n) s).
-    set (l2 := skipn (S n) s).
-
-    (* case distinction on the induction hyptohesis (which is an or) *)
-    destruct IHn as [IHnAllNoMatch | IHnExistsMatch ].
-
-    (* The case where there is already a match with a smaller split. *)
-    2: {
-    right.
-    destruct IHnExistsMatch as [s1 IHn1].
-    destruct IHn1 as [s2 IHn].
-    exists s1. exists s2.
-    destruct IHn as [H0 [H1 [H2 H3]]].
-    repeat split; try assumption.
-    lia. }
-
-
-    (* If none of the earlier splits match. *)
-    destruct (Hdecp l1) as [Hpmatch | Hpnomatch].
-      destruct (Hdecq l2) as [Hqmatch | Hqnomatch ].
-
-      2,3: left;
-      intros s1 s2 Hconcat Hlen;
-      assert (length s1 <= n \/ length s1 = S n) as Hlen' by lia;
-      destruct Hlen' as [Hlen' | Hlen'];
-      try (apply IHnAllNoMatch; assumption); (* case length s1 <= n *)
-      try (
-          destruct (split_list s (S n) s1 s2 Hlen' Hconcat) as [Hfoo Hbar];
-
-          replace l1 with s1 in * by auto;
-          replace l2 with s2 in * by auto;
-          subst;
-          try (left; assumption);
-          try (right; assumption)).
-
-
-    + right. exists l1. exists l2. intros.
-
-      repeat split; try assumption.
-
-      * symmetry. apply firstn_skipn.
-      * apply firstn_le_length.
-
-
-        (* The proof below is the proof for 2,3, but written with periods instead of semicolons...
-           the periods are easier to step through, and it is also how I wrote it.
-           But as I've said above, I don't know how to do that for 2 goals at the same time.
-
-         *)
-        (*
-    +
-      left.
-      intros s1 s2 Hconcat Hlen.
-
-      assert (length s1 <= n \/ length s1 = S n) as Hlen' by lia.
-      destruct Hlen' as [Hlen' | Hlen'].
-
-      * apply IHnAllNoMatch; assumption. (* case length s1 <= n *)
-      * destruct (split_string_lemma s (S n) s1 s2 Hlen' Hconcat) as [Hfoo Hbar].
-
-        replace l1 with s1 in * by auto.
-        replace l2 with s2 in * by auto.
-        subst.
-        try (left; assumption).
-        try (right; assumption).
-*)
-
-Qed.
-
-Lemma denotation_concat_is_decidable (p q: regex):
-  regex_is_decidable p ->
-  regex_is_decidable q ->
-  regex_is_decidable (concat p q).
-Proof.
-  intros Hdecp Hdecq.
-  unfold regex_is_decidable.
-  intro s.
-  destruct (denotation_concat_is_decidable_helper p q Hdecp Hdecq s (length s))
-  as [HAllDontMatch | HExistsMatch].
-
-  - right.
-    unfold not.
-    intro HmatchContr.
-    destruct HmatchContr.
-    symmetry in H.
-
-    set (Hlen := prefix_leq_length s p0 q0 H).
-    unfold no_splitting_is_an_elem_for_length in HAllDontMatch.
-    specialize HAllDontMatch with p0 q0.
-    destruct (HAllDontMatch H Hlen); auto.
-
-  - left.
-    destruct HExistsMatch as [s1 [s2 [Hconcat [Hlen [Hmatchp Hmatchq]]]]].
-    symmetry in Hconcat.
-    destruct_concat_lang.
-    exists s1.
-    exists s2.
-    exists Hconcat.
-    split; assumption.
-Qed.
-
-Lemma denotation_concat_is_decidable_for_empty_string (p q: regex):
-  [] `elem` {{ p }} \/ [] `notelem` {{ p }} ->
-  [] `elem` {{ q }} \/ [] `notelem` {{ q }} ->
-  [] `elem` {{ concat p q }} \/ [] `notelem` {{ concat p q }}.
-Proof.
-intros.
-wreckit.
-- left.
-  destruct_concat_lang.
-  exists [].
-  exists [].
-  exists eq_refl.
-  split; assumption.
-- right.
-  untie.
-  invs H.
-  wreckit.
-  listerine.
-  contradiction.
-- right.
-  untie.
-  invs H.
-  wreckit.
-  listerine.
-  contradiction.
-- right.
-  untie.
-  invs H.
-  wreckit.
-  listerine.
-  contradiction.
-Qed.
-
-Lemma denotation_star_is_decidable_for_empty_string (r: regex):
-  [] `elem` {{ star r }} \/ [] `notelem` {{ star r }}.
-Proof.
-left.
-constructor.
-Qed.
-
-Lemma denotation_is_decidable_on_empty_string (r: regex):
-  [] `elem` {{ r }} \/ [] `notelem` {{ r }}.
-Proof.
-intros.
-induction r.
-- apply denotation_emptyset_is_decidable.
-- apply denotation_lambda_is_decidable.
-- apply denotation_symbol_is_decidable.
-- apply denotation_concat_is_decidable_for_empty_string.
-  + assumption.
-  + assumption.
-- apply denotation_star_is_decidable_for_empty_string.
-- apply denotation_nor_is_decidable.
-  + assumption.
-  + assumption.
 Qed.
 
 Theorem denotation_is_decidable (r: regex) (s: str):
