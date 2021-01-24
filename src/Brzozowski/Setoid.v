fbe2ca4f12f909408f3df9a49c284645faad3a70
Walter Schulze <awalterschulze@gmail.com>
2021-01-24 07:24:09 +0000

    Add Parametric Morphism: elem_morph

--- src/Brzozowski/Setoid.v
+++ src/Brzozowski/Setoid.v
@@ -107,6 +107,29 @@ Qed.
 
 Existing Instance neg_lang_morph_Proper.
 
+Add Parametric Morphism {s}: (elem s)
+  with signature lang_iff ==> iff
+  as elem_morph.
+Proof.
+intros.
+unfold elem.
+unfold "{<->}" in H.
+specialize H with (s := s).
+unfold "\in" in H.
+assumption.
+Qed.
+
+Example example_rewriting_using_lang_iff_in_iff:
+  forall (p q: regex)
+  (pq: {{p}} {<->} {{q}}),
+  forall s: str,
+  s \in {{q}} <-> s \in {{p}}.
+Proof.
+intros.
+rewrite pq.
+reflexivity.
+Qed.
+
 (*
    concat_lang_morph allows rewrite to work inside concat_lang parameters
 *)
